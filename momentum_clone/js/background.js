88faba6471858993116f82778e62a97e5a04b53f
홍준표 <sciencepyo@nate.com>
2022-11-29 10:50:51 +0900

    momentum_css_work

--- momentum_clone/js/background.js
+++ momentum_clone/js/background.js
@@ -2,7 +2,8 @@ const imgs = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg"];
 let img_num = Math.floor(Math.random() * imgs.length);
 
 const bgImage = document.createElement("img");
+bgImage.id = "bgImage";
 
-bgImage.src = `/img/${imgs[img_num]}`;
+bgImage.src = `/momentum_clone/img/${imgs[img_num]}`;
 
 document.body.appendChild(bgImage);
